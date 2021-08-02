37a07ca2ed24b9b37c24496cea3e6ad3c304cdb8
Prem <premmishra17101996@gmail.com>
2021-08-02 20:38:05 +0530

    Added a program where we perform add, substract, multiply and devide in two number

---
+++ Simple Operation.py
@@ -0,0 +1,13 @@
+<html>
+<body>
+<script>
+var a=30;
+var b=20;
+document.write((a+b)+"<br>");
+document.write(a-b+"<br>");
+document.write(a*b+"<br>");
+document.write(a/b);
+</script>
+
+</body>
+</html>
