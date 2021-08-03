52673d618635b7a21b37c3775a3d74d2bb9af90e
Prem <premmishra17101996@gmail.com>
2021-08-03 19:40:55 +0530

    Added a program defined array

---
+++ Array.js
@@ -0,0 +1,10 @@
+<html>
+<body>
+<script>
+firstarray=new Array("Prem","Kumar","Mishra")
+document.write(firstarray[0])
+document.write(firstarray[1])
+document.write(firstarray[2])
+</script>
+</body>
+</html>
