10ca3c289a2ddd5ceacc2b9306c7489c1ed3451f
Prem <premmishra17101996@gmail.com>
2021-08-06 20:39:54 +0530

    Added a js code to find add, substract, devide and multiply by function

---
+++ Function2.js
@@ -0,0 +1,28 @@
+<html>
+<body>
+<script>
+function add(a,b){
+document.write(a+b+"<br>")
+}
+
+function substract(a,b){
+document.write(a-b+"<br>")
+}
+
+function devide(a,b){
+document.write(a/b+"<br>")
+}
+
+function multiply(a,b){
+document.write(a*b)
+}
+
+add(7,8)
+substract(9,6)
+devide(6,3)
+multiply(3,9)
+
+</script>
+
+</body>
+</html>
